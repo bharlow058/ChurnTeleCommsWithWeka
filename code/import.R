684a98f22ffba9f5bb1406091b4729ee69664bc6
Daniel Stanoescu <stanoescu.dan@gmail.com>
2013-02-15 13:41:52 +0000

    Import tools for the .data format for the train and test data.

---
+++ code/import.R
@@ -0,0 +1,19 @@
+# Importing the data to R
+
+# -- Importing train & test set
+
+print("[IMPORT] Importing the train set into R as orange.train...")
+orange.train <- read.delim("../dataset/orange_small_train.data", header=TRUE, sep="\t", fill=TRUE)
+str(orange.train)
+print("[IMPORT] Done!")
+
+print("[IMPORT] Importing the test set into R as orange.test...")
+orange.test  <- read.delim("../dataset/orange_small_test.data", header=TRUE, sep="\t", fill=TRUE)
+str(orange.train)
+print("[IMPORT] Done!")
+
+#TODO - need to clean categorical data from the dataset
+
+
+
+
