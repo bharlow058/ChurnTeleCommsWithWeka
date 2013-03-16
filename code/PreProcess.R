6a107cfa4f4ed822a745eec2d7a8d10acf5b7c0b
Maria Alecu <s1255964@kozlov.inf.ed.ac.uk>
2013-03-16 17:58:32 +0000

    minor modifications to preProcess script, more commentaries, better variable names etc

--- code/PreProcess.R
+++ code/PreProcess.R
@@ -1,20 +1,24 @@
 varsComplete <- paste("Var", 1:230, sep="")
-fmlaComplete <- as.formula(paste("churn ~ ", paste(varsComplete, collapse= "+")))
+
+# Constructing formula for the churn feature
+fmlaChurn <- as.formula(paste("churn ~ ", paste(varsComplete, collapse= "+")))
 
 library(FSelector); # lib.loc="/afs/inf.ed.ac.uk/user/s12/s1255964/programs/Rpackages/");
 
 # Feature Ranking Method for feature selection
-# Only few attributes with big values
-weights_chi    <- chi.squared(fmlaComplete, orange.train.AVG.labels);
-weights_infoGain <- information.gain(fmlaComplete, orange.train.AVG.labels);
-weights_gainRation <- gain.ratio(fmlaComplete, orange.train.AVG.labels); # Returns NaNs
-weigths_sym <- symmetrical.uncertainty(fmlaComplete, orange.train.AVG.labels);
+# Returns only few attributes with big values
+weights_chi    <- chi.squared(fmlaChurn, orange.train.AVG.labels);
+weights_infoGain <- information.gain(fmlaChurn, orange.train.AVG.labels);
+weights_gainRation <- gain.ratio(fmlaChurn, orange.train.AVG.labels); # Returns NaNs
+weigths_sym <- symmetrical.uncertainty(fmlaChurn, orange.train.AVG.labels);
 
-# Lots of attributes with small values
-weights_linear <- linear.correlation(fmlaComplete, orange.train.AVG.labels);
-weights_Spearman <- rank.correlation(fmlaComplete, orange.train.AVG.labels);
+# Returns lots of attributes with small values
+weights_linear <- linear.correlation(fmlaChurn, orange.train.AVG.labels);
+weights_Spearman <- rank.correlation(fmlaChurn, orange.train.AVG.labels);
 
+# Features corresponding to highest values of weights
 features1 <- cutoff.k(weights_chi, 6);
 
 # Subset Selection Method for feature selection 
-features2 <- cfs(fmlaComplete, orange.train.AVG.labels);
+# These methods should return higher quality subsets of features than the previous ones
+features2 <- cfs(fmlaChurn, orange.train.AVG.labels);
