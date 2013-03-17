1c3c4e94a7b7faa6c8b2c22fb80af7e43b5b5500
Maria Alecu <s1255964@kozlov.inf.ed.ac.uk>
2013-03-17 15:25:52 +0000

    feature selection script

---
+++ code/featureSelection.R
@@ -0,0 +1,31 @@
+# Constructing formulas for all categories we want to predict
+varsComplete <- paste("Var", 1:230, sep="")
+
+fmlaChurn     <- as.formula(paste("churn ~ ",     paste(varsComplete, collapse= "+")))
+fmlaAppetency <- as.formula(paste("appetency ~ ", paste(varsComplete, collapse= "+")))
+fmlaUpselling <- as.formula(paste("upselling ~ ", paste(varsComplete, collapse= "+")))
+
+
+# Feature selection using cfs
+library(FSelector, lib.loc="/afs/inf.ed.ac.uk/user/s12/s1255964/programs/Rpackages/");
+# unprocessed data
+features.data.unprocessed.churn     <- cfs(fmlaChurn,     orange.train.NA.labels);
+features.data.unprocessed.appetency <- cfs(fmlaAppetency, orange.train.NA.labels);
+features.data.unprocessed.upselling <- cfs(fmlaUpselling, orange.train.NA.labels);
+
+features.data.unprocessed <- union(features.data.unprocessed.churn,
+                                   union(features.data.unprocessed.appetency,
+                                         features.data.unprocessed.upselling));
+
+# pre-processed data
+features.data.processed.churn     <- cfs(fmlaChurn,     orange.train.AVG.labels);
+features.data.processed.appetency <- cfs(fmlaAppetency, orange.train.AVG.labels);
+features.data.processed.upselling <- cfs(fmlaUpselling, orange.train.AVG.labels);
+
+features.data.processed <- union(features.data.processed.churn,
+                                 union(features.data.processed.appetency,
+                                       features.data.processed.upselling));
+
+# exporting to ARFF
+dme.exportARFF(features.data.unprocessed, "unprocessedReducedDimTrain");
+dme.exportARFF(features.data.processed,   "processedReducedDimTrain")
